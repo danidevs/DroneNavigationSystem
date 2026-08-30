fc946aebea39bc356d177738563fc21c78b3e431
Daniel Rodrigues <danielsk4te@hotmail.com>
2026-08-30 10:00:23 -0300

    Extract drone endpoints from Program

--- src/DroneNavigationSystem.Api/Program.cs
+++ src/DroneNavigationSystem.Api/Program.cs
@@ -1,4 +1,5 @@
 using DroneNavigationSystem.Domain.Entities;
+using DroneNavigationSystem.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,83 +19,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapDroneEndpoints(drone);
 
-var droneApi = app.MapGroup("/api/drone");
 
-droneApi.MapGet("/telemetry", () =>
-{
-    return drone.GetTelemetry();
-})
-.WithName("GetDroneTelemetry")
-
-.WithOpenApi();
-droneApi.MapPost("/takeoff", () =>
-{
-    drone.TakeOff();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("TakeOffDrone")
-.WithOpenApi();
-
-droneApi.MapPost("/land", () =>
-{
-    drone.Land();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("LandDrone")
-.WithOpenApi();
-
-droneApi.MapPost("/move/north", () =>
-{
-    drone.MoveNorth();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("MoveDroneNorth")
-.WithOpenApi();
-droneApi.MapPost("/move/south", () =>
-{
-    drone.MoveSouth();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("MoveDroneSouth")
-.WithOpenApi();
-droneApi.MapPost("/move/east", () =>
-{
-    drone.MoveEast();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("MoveDroneEast")
-.WithOpenApi();
-droneApi.MapPost("/move/west", () =>
-{
-    drone.MoveWest();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("MoveDroneWest")
-.WithOpenApi();
-droneApi.MapPost("/ascend", () =>
-{
-    drone.Ascend();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("AscendDrone")
-.WithOpenApi();
-
-droneApi.MapPost("/descend", () =>
-{
-    drone.Descend();
-
-    return Results.Ok(drone.GetTelemetry());
-})
-.WithName("DescendDrone")
-.WithOpenApi();
 
 
 app.Run();
